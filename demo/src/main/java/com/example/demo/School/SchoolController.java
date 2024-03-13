fc02ffed84cca7308afae410106d9e15eb6dce88
Mendes <161824516+mendesdomingos@users.noreply.github.com>
2024-03-13 20:53:00 -0300

    Created the POST, PUT and DELETE methods

--- demo/src/main/java/com/example/demo/School/SchoolController.java
+++ demo/src/main/java/com/example/demo/School/SchoolController.java
@@ -5,8 +5,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +31,25 @@ public class SchoolController {
     @GetMapping("/{id}")
     public ResponseEntity<Optional<School>> getSchool(@PathVariable Integer id) {
         return ResponseEntity.ok(schoolRepository.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity createSchool(@RequestBody School school) {
+        schoolRepository.save(school);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateSchool(@PathVariable Integer id, @RequestBody School school) {
+        school.setId(id);
+        schoolRepository.save(school);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteSchool(@PathVariable Integer id) {
+        School school = schoolRepository.findById(id).get();
+        schoolRepository.delete(school);
+        return ResponseEntity.ok().build();
     }
 }
